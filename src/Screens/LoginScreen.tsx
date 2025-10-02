ead5f2da6e874eeead473953fe74654011656fcf
afnan khan <afnan.k@dplit.com>
2025-10-02 20:56:59 +0500

    code push

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -48,7 +48,7 @@ function LoginScreen({ navigation }) {
       <View style={styles.spacer} />
       <Text style={styles.label}>Username</Text>
       <TextInput
-        placeholder="Enter your username"
+        placeholder="Enter your username or email"
         value={username}
         onChangeText={setUsername}
         style={styles.input}
