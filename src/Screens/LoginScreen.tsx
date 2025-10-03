6e356eac33aa6c353ca464b95eca8278d613a8dc
afnan khan <afnan.k@dplit.com>
2025-10-03 14:49:42 +0500

    build pushed

--- src/Screens/LoginScreen.tsx
+++ src/Screens/LoginScreen.tsx
@@ -48,7 +48,7 @@ function LoginScreen({ navigation }) {
       <View style={styles.spacer} />
       <Text style={styles.label}>Username</Text>
       <TextInput
-        placeholder="Enter your username for te"
+        placeholder="Enter your username for testing"
         value={username}
         onChangeText={setUsername}
         style={styles.input}
